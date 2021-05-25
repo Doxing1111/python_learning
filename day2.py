a7f5ac000488be20b2c8f6598878e1d02a9184f8
Joe Gou <joegou721@gmail.com>
2021-05-25 22:02:08 +0800

    20210525_2200

--- day2.py
+++ day2.py
@@ -23,17 +23,17 @@ print("%3s  %2d     %3d     %3d     %3d" % ("周星祖", 11, 95, 100, 80))
 
 # ===============================================
 # if 多向判斷式
-# score = int(input('請輸入成績: '))
-# if score >= 90:
-#     print('優等')
-# elif score >= 80:
-#     print('甲等')
-# elif score >= 70:
-#     print('乙等')
-# elif score >= 60:
-#     print('丙等')
-# else:
-#     print('丁等')
+score = int(input('請輸入成績: '))
+if score >= 90:
+    print('優等')
+elif score >= 80:
+    print('甲等')
+elif score >= 70:
+    print('乙等')
+elif score >= 60:
+    print('丙等')
+else:
+    print('丁等')
 
 # ===============================================
 money = int(input('請輸入購物總金額: '))
