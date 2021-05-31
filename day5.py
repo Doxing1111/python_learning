de6f1cd9a3bd76b3bd1f8fca33daa6c8ad45702e
Joe Gou <joegou721@gmail.com>
2021-06-01 03:37:24 +0800

    20210601_0337

--- day5.py
+++ day5.py
@@ -33,6 +33,35 @@ print(n) # 3
 x = list5.pop() # 沒參數則取出最後一個元素
 print(x) # 5
 
+# ======================================================================
+# tuple (元組)
+tuple1 = (1, 2, 3, 4, 5)
+print(len(tuple1)) # 顯示列表元素個數 5
+
+tuple2 = (1, 'Joe', True)
+print(tuple2[1]) # Joe
+
+# 元祖轉換串列, 可新增元素
+tuple3 = (2, False, 'Allen')
+list1 = list(tuple3)
+list1.append('Mark')
+print(list1) # [2, False, 'Allen', 'Mark']
+
+# 串列轉換元祖, 元祖不能增加元素
+# list2 = ['Apple', 'Banana', 'Orange']
+# tuple4 = tuple(list2)
+# tuple4.append(8) # AttributeError: 'tuple' object has no attribute 'append'
+# print(tuple4)
+
+# ==========================================================================
+# dict (字典) 取得元素值以鍵作為索引來取得值
+# dict = {鍵1:值1, 鍵2:值2}
+dict1 = {'Apple':15, 'Banana':25, 'Orange':35}
+print(dict1['Apple']) # 15
+print(dict1['Orange']) # 35
+print(dict1)
+
+
 
 
 
