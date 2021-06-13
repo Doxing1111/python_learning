ec63f2a5da1e9fbd167097ac8852e2c189f28eb5
Joe Gou <joegou721@gmail.com>
2021-06-14 02:13:37 +0800

    20210614_0213

--- day7.py
+++ day7.py
@@ -67,3 +67,48 @@ print(cur_path) # True
 cur_path = os.path.split((__file__))
 print(cur_path) # ('C:/Users/Li/PycharmProjects/python_learning', 'day7.py')
 
+
+# open() 開啟檔案的語法
+# open (檔案名稱[,模式][,編碼])
+# 模式: r(讀取)、w(寫入, 若檔案已存在, 內容會被覆蓋)、a(附加模式, 若檔案已存在, 內容會被附加至尾端)
+
+content = '''Hello Python!
+Learning Python!
+附加測試~
+'''
+
+f = open('test.txt', 'w', encoding='utf-8')
+f.write(content)
+
+content2 = 'TEST'
+f = open('test.txt', 'a')
+f.write(content2)
+
+f = open('test.txt', 'r', encoding='utf-8') # 讀取以UTF-8編碼即可解決 'UnicodeDecodeError: 'cp950' codec can't decode byte 0xe8 in position 7: illegal multibyte sequence' 錯誤
+for line in f:
+    print(line, end="")
+f.close() # 主動關閉
+
+
+# 1 with 敘述結束後, 會自動關閉開啟的檔案
+# 2 with 敘述內的程式必須縮排
+with open('test.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        print(line, end="")
+
+
+# read() 寫法一
+f = open('test.txt', 'r', encoding='utf-8')
+str1 = f.read(5)  # 讀取檔案的前5個字元, 如未輸入長度字元, 則讀取所有的字元
+print(str1)
+f.close()
+
+
+# read() 寫法二
+with open('test.txt', 'r', encoding='utf-8') as f:
+    str1 = f.read(5)
+    print(str1)
+
+
+
+
