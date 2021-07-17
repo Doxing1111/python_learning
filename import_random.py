b453d91286576bb07eb1cf032137ba36cb1c19f8
Joe Gou <joegou721@gmail.com>
2021-07-17 18:37:55 +0800

    20210717_1837

---
+++ import_random.py
@@ -0,0 +1,31 @@
+'''
+date: 2021/07/17
+author: Joe
+'''
+
+''' 產生一個隨機整數1~100
+讓使用者重複輸入數字去猜
+猜對的印出 "恭喜猜中了!"
+猜錯的印出 告訴他比答案小或大'''
+
+import random
+start = input('請決定範圍數字開始值: ')
+end = input('請決定隨機數字範圍結束值: ')
+start = int(start)
+end = int(end)
+
+r = random.randint(start, end)
+count = 0
+while True:
+    count += 1 # count = count + 1
+    num = input('請猜數字: ')
+    num = int(num)
+    if num == r:
+        print('恭喜猜中了!')
+        print('這是你猜的第', count, '次')
+        break
+    elif num > r:
+        print('比答案大')
+    elif num < r:
+        print('比答案小')
+    print('這是你猜的第', count, '次')
